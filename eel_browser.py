1635c417cd4a92ad6742ad789120e97179274c73
Grahulprdxn <rahulg@prdxn.com>
2024-02-19 23:29:48 +0530

    Removed unused codes

--- eel_browser.py
+++ eel_browser.py
@@ -1,16 +1,6 @@
 import eel
-import subprocess
-
 # Set web folder and optionally provide a list of options
 eel.init('web', allowed_extensions=['.js', '.html'])
 
-@eel.expose
-def open_facebook():
-    # Adjust the URL and browser command as needed
-    url = 'https://www.facebook.com'
-    
-    # # Use subprocess to open Brave with the specified URL
-    # subprocess.run(['brave', '--new-window', '--incognito', url])
-
 # Start the Eel app
-eel.start('index.html', size=(800, 600), mode='chrome', port=8080, cmdline_suffix='--new-window --incognito')
+eel.start('index.html', size=(800, 600), mode='chrome', port=8080, cmdline_args=['--incognito', '--no-experiments'])
