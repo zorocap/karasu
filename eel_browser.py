3d99cda0d2f239bed3e1b2cbeafb51c7b7528003
Grahulprdxn <rahulg@prdxn.com>
2024-02-21 21:32:26 +0530

    Removed unwanted code for production

--- eel_browser.py
+++
@@ -1,6 +0,0 @@
-import eel
-# Set web folder and optionally provide a list of options
-eel.init('web', allowed_extensions=['.js', '.html'])
-
-# Start the Eel app
-eel.start('index.html', size=(800, 600), mode='chrome', port=8080, cmdline_args=['--incognito', '--no-experiments'])
--- web/script.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-    window.location.replace("http://localhost:8000/");
-}, 3000);
